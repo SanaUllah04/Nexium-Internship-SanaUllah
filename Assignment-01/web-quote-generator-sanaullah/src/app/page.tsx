1b06e675d621efac0259785240bb38c278d8b76a
Your Name <p229038@pwr.nu.edu.pk>
2025-07-03 18:56:51 +0500

    SHADCN button completely removed.

--- Assignment-01/web-quote-generator-sanaullah/src/app/page.tsx
+++ Assignment-01/web-quote-generator-sanaullah/src/app/page.tsx
@@ -1,12 +1,12 @@
 import Link from 'next/link';
-import ButtonDemo from "./button";
+
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="text-4xl font-bold"> Welcome to the World of Quotes</h1>   
-        <Link href='/quotes'><ButtonDemo /></Link>
+        <Link href='/quotes'><button>Start the Journy</button> </Link>
       </main>  
     </div>
   );
