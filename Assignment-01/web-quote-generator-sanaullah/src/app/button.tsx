4a911280e30b9f4f2da4298913e7a4eb082ec2de
Your Name <p229038@pwr.nu.edu.pk>
2025-07-03 18:49:34 +0500

    separated files for button.

---
+++ Assignment-01/web-quote-generator-sanaullah/src/app/button.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { Button } from "@/components/ui/button";
+
+
+export default function ButtonDemo() {
+  return (
+    <div className="flex flex-wrap items-center gap-2 md:flex-row">
+      <Button>Start the Journy !</Button>
+    </div>
+  )
+}
