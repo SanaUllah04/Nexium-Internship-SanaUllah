5ddd18115763097bdd195473d764f3ffda17d7e8
Your Name <p229038@pwr.nu.edu.pk>
2025-07-03 19:15:28 +0500

    quotes - changes 1.

--- Assignment-01/web-quote-generator-sanaullah/src/app/quotes/page.tsx
+++ Assignment-01/web-quote-generator-sanaullah/src/app/quotes/page.tsx
@@ -1,25 +1,10 @@
+"use client";
+
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card"
 
-export function InputWithButton() {
-  return (
-    <div className="flex w-full max-w-sm items-center gap-2">
-      <Input 
-        type="quoteTopic" 
-        placeholder="Enter your topic..." 
-        className="shadow-sm"
-      />
-      <Button 
-        type="submit" 
-        variant="default"
-        className="shadow-sm hover:shadow-md transition-all"
-      >
-        Generate Quote
-      </Button>
-    </div>
-  )
-}
+
 
 export default function Quote() {
   return (
@@ -38,3 +23,25 @@ export default function Quote() {
     </div>
   )
 }
+
+
+
+function InputWithButton() {
+  return (
+    <div className="flex w-full max-w-sm items-center gap-2">
+      <Input 
+        type="quoteTopic" 
+        placeholder="Enter your topic..." 
+        className="shadow-sm"
+      />
+      <Button 
+        type="submit" 
+        variant="default"
+        className="shadow-sm hover:shadow-md transition-all"
+      >
+        Generate Quote
+      </Button>
+    </div>
+  )
+}
+
